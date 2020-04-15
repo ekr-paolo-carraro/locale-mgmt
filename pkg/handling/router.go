afd17cc6f91c2b3bdc249a7a1176773767182319
paolo.carraro <paolo.carraro@ekr.it>
2020-04-15 16:23:36 +0200

    improvement and better organization of routing

--- pkg/handling/router.go
+++ pkg/handling/router.go
@@ -5,7 +5,6 @@ import (
 	"os"
 
 	"github.com/ekr-paolo-carraro/locale-mgmt/pkg/authorizating"
-	"github.com/ekr-paolo-carraro/locale-mgmt/pkg/session"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,45 +13,34 @@ type Handler struct {
 	PersistenceDelegate interface{}
 }
 
-//NewHandler returnn a new router handler
+//NewHandler return a new router handler
 func NewHandler(delegate interface{}) error {
 
 	rh := gin.Default()
 
-	rh.GET("/welcome", welcomeHandler)
 	rh.GET("/", welcomeHandler)
+	rh.GET("/welcome", welcomeHandler)
 
 	rh.GET("/callback", authorizating.CallbackHandler)
 	rh.GET("/login", authorizating.LoginHandler)
 	rh.GET("/logout", authorizating.LogoutHandler)
-	rh.GET("/info", infoHandler)
+
+	rh.GET("/info", authorizating.InfoHandler)
 
 	apiGroup := rh.Group("/api/v1")
 	{
-		apiGroup.GET("/test", authorizating.AuthRequired(), testApiHandler)
+		apiGroup.GET("/restricted", authorizating.AuthRequired(), authorizating.RestrictedHandler)
 	}
 
 	return rh.Run(os.Getenv("PORT"))
 }
 
-func infoHandler(c *gin.Context) {
-	var msg map[string]interface{} = make(map[string]interface{})
-	msg["version"] = "0.0.1"
-
-	ss, err := session.Store.Get(c.Request, "auth-session")
-	if err != nil {
-		c.AbortWithError(http.StatusInternalServerError, err)
-		return
-	}
-
-	msg["user"] = ss.Values["profile"]
+func welcomeHandler(c *gin.Context) {
+	msg := genericMessage{}
+	msg.Message = "Hi, welcome to locale-mgmt, don't know who you are so go to login"
 	c.JSON(http.StatusOK, msg)
 }
 
-func welcomeHandler(c *gin.Context) {
-	c.String(http.StatusOK, "Hello, server is working: don't know who you are so go to login")
-}
-
-func testApiHandler(c *gin.Context) {
-	c.String(http.StatusOK, "u r in protected area")
+type genericMessage struct {
+	Message string
 }
